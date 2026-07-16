8a2c994f2d17e1a5caae6e30ea7468d3cff97cd7
lshysh626 <lshysh626@users.noreply.github.com>
2026-07-16 16:52:24 +0900

    fix: adjust marker splitting regex in formatModelAnswer to prevent dividing compound index prefixes like 1-1)

--- public/exam/main.js
+++ public/exam/main.js
@@ -92,7 +92,7 @@ function formatModelAnswer(answer, question = '') {
     }
 
     // 1. If it already contains markers like (A), (B), [A], ①, etc., split them into new lines
-    const markerRegex = /([,;\/\s]+)?(\([A-Za-z가-힣0-9]\)|\[[A-Za-z가-힣0-9]\]|[①-⑳]|\b\d{1,2}\)(?=\s+[A-Za-z가-힣\/]))/g;
+    const markerRegex = /([,;\/\s]+)?(\([A-Za-z가-힣0-9]\)|\[[A-Za-z가-힣0-9]\]|[①-⑳]|(?<![\d\-\.\/])\b\d{1,2}\)(?=\s+[A-Za-z가-힣\/]))/g;
     if (markerRegex.test(formatted)) {
         formatted = formatted.replace(markerRegex, (match, sep, marker, index) => {
             return index === 0 ? marker : '\n' + marker;
